5a36e6b2f2491db29f40d1cdaeaf203fc6bb2756
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2022-04-05 14:05:28 +0530

    Create Lab2

---
+++ Lab2.py
@@ -0,0 +1,48 @@
+# # LAB 2 :
+print("Hello world")
+
+list1 = [1,2,3,4,5,6]
+list2 = [4,5,6,7,8,9]
+
+
+# ## Method 1 : using + operator
+
+print("Method 1 : using + operator")
+joined_list1 = list1 + list2
+print("Concatenation of list1 : {} and list2 : {} is {}\n" .format(list1,list2,joined_list1))
+
+
+# ## Method 2 : Using * operator
+
+print("Method 2 : Using * operator")
+joined_list2 = [*list1, *list2]
+print("Concatenation of list1 : {} and list2 : {} is {}\n" .format(list1,list2,joined_list2))
+
+
+# ## Method 3 : Using extend() function
+
+print("Method 3 : Using extend() function")
+joined_list3 = list2.extend(list1)
+print("Concatenation of list1 : {} and list2 : {} is {}\n" .format(list1,list2,joined_list3))
+
+
+# ## Method 4 : Using itertools.chain
+
+print("Method 4 : Using itertools.chain")
+import itertools
+joined_list4 = list(itertools.chain(list1, list2))
+print("Concatenation of list1 : {} and list2 : {} is {}\n" .format(list1,list2,joined_list4))
+
+
+# ## Method 5 : using append function
+
+print("Method 5 : using append function")
+for value in list2:
+    list1.append(value)
+print("Concatenation of list1 : {} and list2 : {} is {}\n" .format(list1,list2, list2))
+
+
+# ## UNION of two list
+ 
+ joined_list5 = list(set(list1+list2))
+ print("Union of list 1 and list 2 =", joined_list5)
